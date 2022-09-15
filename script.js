96fad9349db1bfaabcef26df7cdced5d74f6cb84
Jusi <megamahmutpower@gmail.com>
2022-09-15 19:48:02 +0300

    last change

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let burger = document.querySelector('.burger');
+let nav__link = document.querySelector('.nav__link');
 
 burger.onclick = function(){
     burger.classList.toggle('active__burger');
+    nav__link.classList.toggle('active__panel');
+}
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
 }
